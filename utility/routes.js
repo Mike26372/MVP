f0fa6ceb54393e23bbe55c2d4c70dce791c460ee
Michael Tutt <tutt.michael@gmail.com>
2017-01-23 11:06:18 -0800

    Add utility and package.json files

---
+++ utility/routes.js
@@ -0,0 +1,13 @@
+
+var listController = require('../controllers/listController.js');
+
+
+module.exports = function(app, express) {
+  app.get('/list', listController.getAllTodos);
+  app.post('/list', listController.addToList);
+
+  app.use(function(req, res) {
+    console.log('Unhandled server request');
+    console.log(req.body);
+  });
+}
---
+++ utility/utility-func.js
@@ -0,0 +1,5 @@
+// Use utility functions to connect React with Mongo?
+var Q = require('q');
+var List = require('./schema/list.js');
+var User = require('./schema/user.js');
+
